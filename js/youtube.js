d3f9b8caf3e932f7fdfe6d243ba02389f35fade2
ohtaekwon <ohtk1119@gmail.com>
2022-06-10 16:03:16 +0900

    Youtubu수정

--- js/youtube.js
+++ js/youtube.js
@@ -4,6 +4,12 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+const YOUTUBE = ["J0pkfjlaxYI", "5v4SfDWIG5Q", "AuJXFfkUjDU", "K4pQSRS__cs"];
+
+function random() {
+  randomVedio = Math.floor(Math.random() * YOUTUBE.length);
+  return YOUTUBE[randomVedio];
+}
 // onYouTubePlayerAPIReady 함수 이름은,
 // Youtube IFrame Player API에서 사용하는 이름이기 때문에,
 // 다르게 지정하면 동작하지 않습니다!
@@ -11,12 +17,15 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubePlayerAPIReady() {
   // 절대 바꾸면 안됨
   // <div id="player"></div>
+  const movieID = random();
+  console.log(movieID);
+
   new YT.Player("player", {
-    videoId: "AuJXFfkUjDU", // 최초 재생할 유튜브 영상 ID
+    videoId: movieID, // 최초 재생할 유튜브 영상 ID
     playerVars: {
       autoplay: true, // 자동 재생 유무
       loop: true, // 반복 재생 유무
-      playlist: "AuJXFfkUjDU", // 반복 재생할 유튜브 영상 ID 목록
+      playlist: movieID, // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
       // 영상이 준비되었을 때,
